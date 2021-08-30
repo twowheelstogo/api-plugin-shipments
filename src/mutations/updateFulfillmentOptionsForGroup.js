44e26d536da8a99b887f439a3a5210fcda0421c8
diego jose orellana bojorquez <diegoorellana@twowheelstogo.com>
2021-08-30 08:44:06 -0600

    add logic of circles

--- src/mutations/updateFulfillmentOptionsForGroup.js
+++ src/mutations/updateFulfillmentOptionsForGroup.js
@@ -80,22 +80,35 @@ export default async function updateFulfillmentOptionsForGroup(context, input) {
   // In the future we want to do this async and subscribe to the results
   const rates = await context.queries.getFulfillmentMethodsWithQuotes(commonOrder, context);
 
-  const { shipmentQuotes, shipmentQuotesQueryStatus } = getShipmentQuotesQueryStatus(rates);
-  const currentFulfillment = cart.shipping.find((group) => group._id === fulfillmentGroupId);
-    console.log("currentFulfillment",currentFulfillment);
-    if(currentFulfillment.type === "pickup") {
+  const { shipmentQuotes, shipmentQuotesQueryStatus, address } = getShipmentQuotesQueryStatus(rates);
+  const currentFulfillment = cart.shipping.find((group) => group._id === fulfillmentGroupId);  
+  if(currentFulfillment.type === "pickup") {
       const freeQuote = shipmentQuotes.find((group) => group.method.group === "Free");
-      console.log("freeQuote",freeQuote)
       if(freeQuote) {
         await context.mutations.selectFulfillmentOptionForGroup(context,{
           cartId,
           cartToken,
           fulfillmentGroupId,
           fulfillmentMethodId: freeQuote?.method._id
-        })
-      }
-      
+        });
     }
+  }else{
+    const groundQuotes = shipmentQuotes.filter((group) => group.method.group === "Ground");
+    if(groundQuotes.length == 0){
+      throw new ReactionError("not-found-ground-quotes", `No se ha agregado los métodos de cobros de envíos`);
+    }
+    groundQuotes.sort((a, b) => a.handlingPrice-b.handlingPrice);
+    let circleQuote = groundQuotes.find((group) => address.metaddress.distance.value <= group.handlingPrice);
+    if(!circleQuote){
+      throw new ReactionError("limit-exceeded-on-ground-quotes", `La distancia está fuera del límite máximo (${groundQuotes[groundQuotes.length-1].handlingPrice} kms), selecciona el método de pickup`);
+    }
+    await context.mutations.selectFulfillmentOptionForGroup(context,{
+      cartId,
+      cartToken,
+      fulfillmentGroupId,
+      fulfillmentMethodId: circleQuote.method._id
+    });
+  }
   if (!_.isEqual(shipmentQuotes, fulfillmentGroup.shipmentQuotes) || !_.isEqual(shipmentQuotesQueryStatus, fulfillmentGroup.shipmentQuotesQueryStatus)) {
     const updatedCart = {
       ...cart,
